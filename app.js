fff3222f215e91a566784063b193b71c660fa44c
Jatin Thakur <73630627+JatinThakur-12@users.noreply.github.com>
2023-04-11 11:20:29 +0530

    v-1.0.0 username & password with ercyption and env

---
+++ app.js
@@ -0,0 +1,80 @@
+require("dotenv").config()
+const express= require("express");
+const bodyParser= require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+const PORT= process.env.PORT || 3000;
+//Connecting DataBase and laying its structure
+mongoose.connect("mongodb://127.0.0.1:27017/userDB").then(()=>{
+    console.log("Connected to database successfully");
+});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields: ['password'] });
+
+
+const User = mongoose.model("user", userSchema);
+
+
+//ROUTING PATHS
+
+////////////////////////// Root Route //////////////////////////////
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+////////////////////////// Login Route /////////////////////////////
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.post("/login",function(req,res){
+    const username= req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email : username}).then((foundUser)=>{
+        if(foundUser.password === password){
+            console.log("User Found");
+            res.render("secrets");
+        }
+        else{
+            res.write("<h1>Oops Wrong password! Try Again</h1>");
+            res.send();
+        }
+    });
+
+});
+
+///////////////////////// Register Route ///////////////////////////
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+app.post("/register",function(req,res){
+    const newUser = new User({
+        email : req.body.username,
+        password : req.body.password
+    });
+
+    newUser.save().then(()=>{
+        console.log("Registraton Successfull");
+        res.render("secrets");
+    });
+    
+
+});
+
+app.listen(PORT,function(req,res){
+    console.log("Server is running on "+PORT);
+});
