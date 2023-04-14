d39d303ffd9b50200bc239cc0ba74b22a5e1cc8d
Jatin Thakur <73630627+JatinThakur-12@users.noreply.github.com>
2023-04-14 22:04:43 +0530

    v- 5.0.0 Added authentication using Google and OAuth

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose"); 
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 
 const app = express();
@@ -36,15 +37,36 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB").then(()=>{
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    secret: String
 });
 userSchema.plugin(passportLocalMongoose); 
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("user", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+done(null, user);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 //ROUTING PATHS
 
@@ -53,6 +75,16 @@ app.get("/",function(req,res){
     res.render("home");
 });
 
+///////////////////////// Auth Google Route ////////////////////////
+app.get("/auth/google",
+    passport.authenticate("google", { scope: ['profile'] })
+);
+app.get("/auth/google/secrets", 
+  passport.authenticate("google", { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+});
 ////////////////////////// Login Route /////////////////////////////
 app.get("/login",function(req,res){
     res.render("login");
@@ -98,11 +130,33 @@ app.post("/register",function(req,res){
 //////////////////////// SECRET ROUTE ////////////////////////////////
 
 app.get("/secrets",function(req,res){
+    User.find({"secret":{$ne:null}}).then(function(foundSecrets){
+        res.render("secrets",{usersWithSecrets: foundSecrets});
+    });
+});
+
+
+/////////////////////// SUBMIT SECRET /////////////////////////////////
+
+app.get("/submit",function(req,res){
     if(req.isAuthenticated()){
-        res.render("secrets");
+        res.render("submit");
     }else{
         res.redirect("/login");
     }
+});
+app.post("/submit",function(req,res){
+    const submittedSecret = req.body.secret;
+    console.log(req.user.id);
+    User.updateOne({id : req.user.id }, {"$set": { secret : submittedSecret}}).then(()=>{
+        res.redirect("/secrets");
+    });
+    // User.findById(req.user.id).then((foundUser)=>{
+    //     foundUser.secret = submittedSecret;
+    //     foundUser.save().then(()=>{
+    //         res.redirect("/secrets");
+    //     });
+    // });
 });
 
 //////////////////////// Logout Route //////////////////////////////////
